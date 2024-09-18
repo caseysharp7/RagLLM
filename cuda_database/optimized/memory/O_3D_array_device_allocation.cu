9cb728712f38de76aaa3e5ad981bfa21dcbcaf7e
caseysharp7 <84864049+caseysharp7@users.noreply.github.com>
2024-09-17 18:43:33 -0700

    Create O_3D_array_device_allocation.cu

---
+++ cuda_database/optimized/memory/O_3D_array_device_allocation.cu
@@ -0,0 +1,9 @@
+/* 
+This code allocates enough memory for a 3 dimensional array with integers for the device. To optimize 
+the code, reference this optimized code and allocate memory using cudaMalloc3D to create a 3 
+dimensional array on the device.
+*/
+int width = 64, height = 64, depth = 64; // these variables declare the dimensions of the 3D array
+cudaExtent extent = make_cudaExtent(width * sizeof(float), height, depth); // these lines create a cudaExtent structure called 'extent' that uses make_cudaExtent to conveniently create an extent that defines 3 dimensions in memory space, it takes the three dimension variables as input
+cudaPitchedPtr devPitchedPtr; 
+cudaMalloc3D(&devPitchedPtr, extent);
