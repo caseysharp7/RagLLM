2b137481e8a8fa56cc948e1bf77902995b52083c
caseysharp7 <84864049+caseysharp7@users.noreply.github.com>
2024-10-12 11:27:36 -0700

    Update host_to_from_device_memory.cu

--- cuda_database/unoptimized/data_flow/host_to_from_device_memory.cu
+++ cuda_database/unoptimized/data_flow/host_to_from_device_memory.cu
@@ -1,5 +1,3 @@
-// This is my own code, though it is closely related to the optimized example
-
 for(int i = 0; i < 2; ++i){
     cudaMemcpy(inputDevPtr + i * size, hostPtr + i * size, size, cudaMemcpyHostToDevice);
 }
