7b58f1c45af1de1e2cae20b4afab0f920d50f89d
Casey Sharp <cjsharp7@gmail.com>
2025-09-05 22:25:35 -0700

    Update cuda_json_file_convert.py

--- file_sort/cuda_json_file_convert.py
+++ file_sort/cuda_json_file_convert.py
@@ -1,10 +1,9 @@
 import json
 import os
 
-with open('/home/sharpcj/cuda_links.json', 'r') as file:
+with open('/home/cuda_links.json', 'r') as file:
     links = json.load(file)
 
-# print(os.path.abspath('O_2D_array_device_allocation.cu'))
 
 for i in links:
     one = i["2D_array_device_allocation"]
@@ -14,7 +13,6 @@ for i in links:
     print(f"Linking {one} with {two}")
     print(f"Description: {desc}")
 
-    # if os.path.exists("/home/sharpcj/my_cuda/unoptimized/memory/2D_array_allocation.cu"):
     with open(one, 'r') as file_1, open(two, 'r') as file_2:
         code_1 = file_1.read()
         code_2 = file_2.read()
